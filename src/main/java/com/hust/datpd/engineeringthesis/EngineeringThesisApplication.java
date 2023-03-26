905f36fb76ac4938416d73f67659088c00bad479
phamducdat <phamducdat2402@gmail.com>
2023-03-26 09:40:35 +0700

    init

---
+++ src/main/java/com/hust/datpd/engineeringthesis/EngineeringThesisApplication.java
@@ -0,0 +1,13 @@
+package com.hust.datpd.engineeringthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust/datpd/engineeringthesis/EngineeringThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.datpd.engineeringthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
